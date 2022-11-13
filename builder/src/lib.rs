52714bb049f721270619f8eb4d3895885e53a1df
Tobeyyyyy <tobiasscholz2000@gmail.com>
2022-11-13 01:30:11 +0100

    abc

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,122 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+fn ty_inner_type(ty: &syn::Type) -> Option<&syn::Type> {
+    if let syn::Type::Path(ref p) = ty {
+        if p.path.segments.len() != 1 || p.path.segments[0].ident != "Option" {
+            return None;
+        }
+
+        if let syn::PathArguments::AngleBracketed(ref inner_ty) = p.path.segments[0].arguments {
+            if inner_ty.args.len() != 1 {
+                return None;
+            }
+
+            let inner_ty = inner_ty.args.first().unwrap();
+            if let syn::GenericArgument::Type(ref t) = inner_ty.value() {
+                return Some(t);
+            }
+        }
+    };
+    None
+}
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = syn::Ident::new(&bname, name.span());
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        unimplemented!();
+    };
+
+    let optionized = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if ty_inner_type(ty).is_some() {
+            quote! {
+                #name: #ty
+            }
+        } else {
+            quote! {
+                #name: std::option::Option<#ty>
+            }
+        }
+    });
+
+    let methods = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if let Some(inner_ty) = ty_inner_type(ty) {
+            quote! {
+                pub fn #name(&mut self, #name: #inner_ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        }
+    });
+
+    let build_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if ty_inner_type(ty).is_some() {
+            quote! {
+                #name: self.#name.clone()
+            }
+        } else {
+            quote! {
+                #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+            }
+        }
+    });
+
+    let build_empty = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! {
+            #name: None
+        }
+    });
+
+    let expanded = quote!(
+        pub struct #bident {
+            #(#optionized,)*
+        }
+
+        impl #bident {
+            #(#methods)*
+
+            pub fn build(&self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok(#name {
+                    #(#build_fields,)*
+                })
+            }
+        }
+
+        impl #name {
+            fn builder() -> #bident {
+                #bident {
+                     #(#build_empty,)*
+                }
+            }
+        }
+    );
+
+    expanded.into()
 }
